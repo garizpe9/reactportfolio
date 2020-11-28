1a5465cebf6494acdf45aafee1b16369211cb061
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-11-28 12:39:11 -0600

    update

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Nav from "./component/Nav";
+import Jumbotron from "./component/Jumbotron"
+import Footer from "./component/Footer"
+
+
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Jumbotron>
+      </Jumbotron>
+      <Footer />
+
+    </div>
+  );
+}
+
+
+export default App;
---
+++ client/src/component/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {CardFooter  } from 'reactstrap';
+
+import { faFilePdf,faFolderOpen,faEnvelopeOpen,faCopyright} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+export const Footertest = () => {
+    return(
+<div>
+    <CardFooter>
+    <FontAwesomeIcon icon={faFilePdf} size="2x" />
+    <FontAwesomeIcon icon={faEnvelopeOpen} size="2x" />
+    <FontAwesomeIcon icon={faFolderOpen} size="2x" />
+    <FontAwesomeIcon icon={faCopyright} size="2x" />
+    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+    <FontAwesomeIcon icon={faGithub} size="2x" />
+    </CardFooter>
+  </div>
+    );
+};
+
+export default Footertest;
+
+
