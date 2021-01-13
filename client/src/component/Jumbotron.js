1a2264d7cc324b5d49d28759296af625fc4bce5e
Giovanna Arizpe <veliaarizpe@gmail.com>
2021-01-12 21:18:15 -0600

    update with project eight

--- client/src/component/Jumbotron.js
+++ client/src/component/Jumbotron.js
@@ -11,9 +11,9 @@ const Example = (props) => {
         <p className="lead">About Me</p>
         <hr className="my-2" />
         <img className="picture" src= {require ("../component/assets/IMG_1589.jpg")} alt="card"/> <br/>
-        <p>I am a Full Stack developer with tax provision implementation knowledge educated at University of Florida, Loyola University of Chicago, and Northwestern University. My skills range from HTML, CSS, JavaScript, Node.js, MySQL, MongoDB, React to ERM systems and Tax Provision and compliance software with strengths in problem solving, creativity, and teamwork.<br/><br/>
+        <p>I am a Full Stack Software Architect with tax provision implementation knowledge educated at University of Florida, Loyola University of Chicago, and Northwestern University. My skills range from HTML, CSS, JavaScript, Node.js, MySQL, MongoDB, React to ERM systems and Tax Provision and compliance software with strengths in problem solving, creativity, and teamwork.<br/><br/>
 
-        I am passionate about learning new technologies and applying skills to improve overall efficiencies of projects and sharing information with others. Not only do I enjoy showing clients how technology works, but ensure to take time after troubleshooting to demonstate to clients how to solve the issue themselves so they can better maintain their software environment. <br/><br/>
+        I am passionate about learning new technologies and applying skills to improve project efficiencies. I  also enjoy showing clients how technology works and I take time after troubleshooting to demonstate to clients how to solve the issue themselves so they can better maintain their software environment. <br/><br/>
         I strive to enhance internal and external processes and created company guidelines on how to update software along with company instructional guides for clients to use on tax software. Moreover, I implemented proofs for Tax Analytics applications and created several applications based on the company's code library. <br/><br/>
 
          I am excited to develop responsive websites and am positioned to provide unique perspectives on how end users interact with websites and software platforms by leveraging my background in professional services in a Tax Provision software company.</p>
---
+++ client/src/component/projects/ProjectEight.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Card, Button, CardHeader, CardFooter, CardBody,
+  CardTitle, CardText } from 'reactstrap';
+  import {faGithub} from '@fortawesome/free-brands-svg-icons';
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  
+
+const ProjectSeven = (props) => {
+  return (
+    <div>
+ <div>
+
+      
+<Card>
+  <CardHeader tag="h3" >Train of Thought</CardHeader>
+  <CardBody>
+    <CardTitle tag="h5"></CardTitle>
+    <CardText>Train of Thought implores users to sit down and elaborate on their thoughts and feelings on any subject. With our journal entry section, users have free reign to vent out any frustrations or highlight special parts of their day.<br/><br/>
+    Train of Thought includes a mood recognition page that detects a user's mood by using their camera and curates prompts for those who need some guidance on how to externalize their temperament.<br/><br/>
+
+    This app uses the following technologies: MERN stack, passport.js, face-api.js, material-auto-rotating-carousel.js, material-ui.js, react-swipeable-views.js, styled-components.js, Healthroughwords API.</CardText>
+    <img className="profile" src= {require ("../assets/TOT1.jpg")} alt="card"/>
+    <img className="profile" src= {require ("../assets/TOT2.jpg")} alt="card"/>
+    <img className="profile" src= {require ("../assets/TOT3.jpg")} alt="card"/>
+    <Button size="sm" href="https://this-face-space.herokuapp.com/">Visit Page</Button>
+  </CardBody>
+  <CardFooter className="text-muted"><a href="https://github.com/garizpe9/facespace">View Repo <FontAwesomeIcon  icon={faGithub}/> </a></CardFooter>
+</Card>
+
+</div>
+    </div>
+  );
+};
+
+export default ProjectSeven;
