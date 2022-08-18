f45e64b6cd7b32adc92ac49ae445684819c5d438
Giovanna Arizpe <veliaarizpe@gmail.com>
2022-08-18 11:45:19 -0500

    Update Jumbotron.js

--- client/src/component/Jumbotron.js
+++ client/src/component/Jumbotron.js
@@ -14,7 +14,7 @@ const Example = (props) => {
         <p>I am a Full Stack Software Architect with tax provision implementation knowledge educated at University of Florida, Loyola University of Chicago, and Northwestern University. My skills range from HTML, CSS, JavaScript, Node.js, MySQL, MongoDB, React to ERM systems and Tax Provision and compliance software with strengths in problem solving, creativity, and teamwork.<br/><br/>
 
         I am passionate about learning new technologies and applying skills to improve project efficiencies. I  also enjoy showing clients how technology works and I take time after troubleshooting to demonstate to clients how to solve the issue themselves so they can better maintain their software environment. <br/><br/>
-        I strive to enhance internal and external processes and created company guidelines on how to update software along with company instructional guides for clients to use on tax software. Moreover, I implemented proofs for Tax Analytics applications and created several applications based on the company's code library. <br/><br/>
+        I strive to enhance internal and external processes and created company guidelines on how to update software along with company instructional guides for clients to use on tax software.  <br/><br/>
 
          I am excited to develop responsive websites and am positioned to provide unique perspectives on how end users interact with websites and software platforms by leveraging my background in professional services in a Tax Provision software company.</p>
         <p className="lead">
